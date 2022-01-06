b15a4ec053d6096ffe54a120c061732610370157
Emily Burger <eburger939@gmail.com>
2022-01-06 14:18:15 -0600

    added prompt questions, template html and style sheet

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
+const template = requie('template.html')
+
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: "Team member's name",
+      name: 'name',
+    },
+      {
+        type: 'input',
+        message: 'Employee ID:',
+        name: 'ID',
+      },
+      {
+        type: 'input',
+        message: 'Employee email address:',
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'Employee office number: ',
+        name: 'number',
+      },
+      
+  ])
+  .then((data) =>
+
+    ));
