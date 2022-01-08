19d4625fae2eeccf55eb4112afa279a11deb6598
Emily Burger <eburger939@gmail.com>
2022-01-07 19:22:58 -0600

    added correct file structue, created all classes, set up all inquirer prompts and build team array

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
-const template = requie('template.html')
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern')
+const Engineer = require('./lib/engineer')
 
+// const template = require('template.html')
 
+let team = [];
 
-inquirer
+function init() {
+  inquirer
   .prompt([
     {
       type: 'input',
@@ -28,6 +33,104 @@ inquirer
       },
       
   ])
-  .then((data) =>
+  .then((data) => {
+      const manager = new Manager(data.name, data.ID, data.email, data.number)
+      team.push(manager);
+      teamMember()
+  });
+}
+init();
 
-    ));
+    function teamMember() {
+      inquirer.prompt([
+        {
+          type: "list",
+          message:"What team memeber would you like to create?",
+          name: "option",
+          choices:["Engineer", "Intern", "Exit & Build team!"]
+        }
+      ]).then(function(answer){
+        switch (answer.option) {
+          case "Engineer":
+            newEngineer();
+            break;
+          case "Intern":
+            newIntern();
+            break; 
+          default:
+            buildTeam();
+        }
+      })
+    }
+
+
+    function newEngineer() {
+      inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: "Team member's name",
+          name: 'name',
+        },
+          {
+            type: 'input',
+            message: 'Employee ID:',
+            name: 'ID',
+          },
+          {
+            type: 'input',
+            message: 'Employee email address:',
+            name: 'email',
+          },
+          {
+            type: 'input',
+            message: 'What is your GitHub username?',
+            name: 'github',
+          },
+          
+      ])
+      .then((data) => {
+          const engineer = new Engineer(data.name, data.ID, data.email, data.github)
+          team.push(engineer);
+          teamMember()
+      });
+      
+      
+    }
+
+    function newIntern() {
+      inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: "Team member's name",
+          name: 'name',
+        },
+          {
+            type: 'input',
+            message: 'Employee ID:',
+            name: 'ID',
+          },
+          {
+            type: 'input',
+            message: 'Employee email address:',
+            name: 'email',
+          },
+          {
+            type: 'input',
+            message: 'What school do you attend?',
+            name: 'school',
+          },
+          
+      ])
+      .then((data) => {
+          const intern = new Intern(data.name, data.ID, data.email, data.school)
+          team.push(intern);
+          teamMember()
+      });
+      
+    }
+
+    function buildTeam() {
+      console.log(team)  
+    }
